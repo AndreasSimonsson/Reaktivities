efc070946ee765c7ddbc8d101610eb3c1d19a98f
Andreas Simonsson <andreas.simonsson@gmail.com>
2020-05-05 11:25:44 +0200

    Added react ap

--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -18,7 +18,7 @@ namespace Persistance
             builder.Entity<Value>().HasData(
                 new Value {Id = 1, Name = "101"},
                 new Value {Id = 2, Name = "102"},
-                new Value {Id = 3, Name = "103"}
+                new Value {Id = 3, Name = "104"}
             );
         }
     }
