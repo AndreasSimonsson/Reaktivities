a4057d4eb16437238a06d660a8e4aac97d1bd6c1
Andreas Simonsson <andreas.simonsson@gmail.com>
2020-05-06 09:59:53 +0200

    End of stage 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,6 +29,13 @@ namespace API
             services.AddDbContext<Persistance.DataContext>(
                 opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
+
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -42,6 +49,8 @@ namespace API
 
             //app.UseHttpsRedirection();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseRouting();
 
             app.UseAuthorization();
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Header, Icon, List } from 'semantic-ui-react'
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends Component {
+
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    // this.setState({
+    //   values: [{id:1, name:"Value 101"}, {id:2, name:"Value 202"}]
+    // })
+
+    Axios.get("http://localhost:5000/api/values").then(result => {
+      this.setState({values: result.data })
+    }) 
+    
+  }
+
+  render()
+  {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+        {
+          this.state.values.map((value:any) => 
+            (<List.Item key={value.id}>{value.name}</List.Item>)
+            )
+        }
+        </List>
+      </div>
+    );
+  }
 }
 
 export default App;
