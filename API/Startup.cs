ec440e1d294b6bd343f4bb9441c7f395a8c8ef69
Andreas Simonsson <andreas.simonsson@gmail.com>
2020-06-01 14:04:32 +0200

    End of section 12.

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,16 @@ using MediatR;
 using FluentValidation.AspNetCore;
 using Application.Activities;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Persistance;
+using Application.Interfaces;
+using Infrastructure.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
@@ -26,15 +36,43 @@ namespace API
                 opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
 
-            services.AddCors(opt =>{
-                opt.AddPolicy("CorsPolicy", policy => {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
 
             services.AddMediatR(typeof(Application.Activities.List).Assembly);
-            services.AddControllers()
+
+            services.AddControllers(opt=>{
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
             .AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Create>());
+
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services)
+            .AddEntityFrameworkStores<DataContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateAudience = false,
+                    ValidateIssuer = false
+                };
+            });
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,10 +88,10 @@ namespace API
 
             //app.UseHttpsRedirection();
 
-            app.UseCors("CorsPolicy");
-            
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -1,9 +1,11 @@
 ﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistance
 {
-    public class DataContext : DbContext
+    //Adding Appuser is indata to  IdentityDbContext => no need for adding DbSet for AppUSer
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
@@ -15,6 +17,8 @@ namespace Persistance
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+
             builder.Entity<Value>().HasData(
                 new Value {Id = 1, Name = "101"},
                 new Value {Id = 2, Name = "102"},
